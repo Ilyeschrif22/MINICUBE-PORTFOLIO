47af2cac06157b8bd10a768badec1cba54243946
Ilyeschrif22 <ilyes.chrif@esprit.tn>
2025-11-16 00:15:56 +0100

    design&update&humburgerMenu

--- script.js
+++ script.js
@@ -1,41 +1,12 @@
-const hamMenu = document.getElementById('hamMenu');
-const hamIcon = document.getElementById('hamIcon');
-const hamMenuList = document.getElementById('hamList')
-const statueImage = document.getElementById('statueImage')
-count = 1;
+console.log("js")
+const logo = document.getElementById('hamburger-icon');
+const panel = document.getElementById('slidePanel');
+const closeBtn = document.getElementById('closeBtn');
 
-hamMenu.addEventListener('click', function (e) {
-    e.stopPropagation();
-    hamMenu.classList.toggle('active');
-
+logo.addEventListener('click', () => {
+    panel.classList.toggle('active');
 });
 
-const menuLinks = document.querySelectorAll('.menu-content a');
-menuLinks.forEach(link => {
-    link.addEventListener('click', function () {
-        hamMenu.classList.remove('active');
-        hamIcon.classList.remove('active');
-
-    });
+closeBtn.addEventListener('click', () => {
+    panel.classList.remove('active');
 });
-
-
-
-/* ham menu */
-hamMenu.addEventListener('click', () => {
-})
-
-hamMenu.addEventListener('click', () => {
-    if (count % 2 === 0) {
-        hamMenuList.classList.add('hidden');
-        count++;
-    }
-    else {
-        setTimeout(() => {
-            hamMenuList.classList.remove('hidden');
-        }, 500);
-        count++;
-    }
-})
-
-
